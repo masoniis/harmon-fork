8c54b89d9b564f3a72f3d7c15c303c37fc271bbe
dane <danerieber@gmail.com>
2024-04-24 20:58:39 -0500

    fix textarea focus on other users' chat messages

--- index.ts
+++ index.ts
@@ -286,11 +286,11 @@ const server = Bun.serve({
                 <div class="chat_message_content" hx-disable>${content}</div>
               </div>
             </div>
-            ${newMessageTextarea}
           `,
         );
         ws.sendText(html`
             <input id="session_token" name="session_token" type="hidden" value="${stoken}" hx-swap-oob="true"></input>
+            ${newMessageTextarea}
           `);
 
         prevMessageUsername = username;
