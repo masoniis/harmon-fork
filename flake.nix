ddfc8dc6f6dafb8d156d5337cab30f5f711bdf49
dane <danerieber@gmail.com>
2024-04-24 20:40:12 -0500

    remove bun overlay

--- flake.nix
+++ flake.nix
@@ -2,21 +2,7 @@
   inputs = { utils.url = "github:numtide/flake-utils"; };
   outputs = { self, nixpkgs, utils }:
     utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            (self: super: {
-              bun = super.bun.overrideAttrs (final: prev: {
-                src = pkgs.fetchurl {
-                  url =
-                    "https://github.com/oven-sh/bun/releases/download/bun-v${prev.version}/bun-linux-x64-baseline.zip";
-                  hash = "sha256-EKl1neLZfjfSNax2HDFSiO4XGuxMDR6Mom8NnVq7DSY=";
-                };
-              });
-            })
-          ];
-        };
+      let pkgs = import nixpkgs { inherit system; };
       in {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
