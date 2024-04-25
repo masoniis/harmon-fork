a6259ddada54fd6278a5198dde2c836127485d56
dane <danerieber@gmail.com>
2024-04-24 20:18:15 -0500

    fix nix package, use bun baseline build

--- default.nix
+++ default.nix
@@ -1,21 +1,47 @@
-{ lib, pkgs, stdenv }:
-
-stdenv.mkDerivation {
-  pname = "harmon";
-  version = "pre-alpha";
-
+{ stdenv, lib, bun, makeBinaryWrapper }:
+let
   src = ./.;
+  version = "pre-alpha";
+  node_modules = stdenv.mkDerivation {
+    pname = "haron-node_modules";
+    inherit src version;
+    impureEnvVars = lib.fetchers.proxyImpureEnvVars
+      ++ [ "GIT_PROXY_COMMAND" "SOCKS_SERVER" ];
+    nativeBuildInputs = [ bun ];
+    dontConfigure = true;
+    buildPhase = ''
+      bun install --no-progress --frozen-lockfile
+    '';
+    installPhase = ''
+      mkdir -p $out/node_modules
 
-  nativeBuildInputs = with pkgs; [ bun makeBinaryWrapper wget ];
+      cp -R ./node_modules $out
+    '';
+    outputHash = "sha256-AbL6fSX9Vweh0lbthPvMqI4u1A6cV9wc53g+5MWKR6c=";
+    outputHashAlgo = "sha256";
+    outputHashMode = "recursive";
+  };
+in stdenv.mkDerivation {
+  pname = "harmon";
+  inherit src version;
+  nativeBuildInputs = [ makeBinaryWrapper ];
 
   dontConfigure = true;
-  buildPhase = ''
-    bun install --no-progress --frozen-lockfile
-  '';
+  dontBuild = true;
+
   installPhase = ''
-    cp -r . $out
-    makeBinaryWrapper ${pkgs.bun}/bin/bun $out/bin/harmon \
-      --prefix PATH : ${lib.makeBinPath [ pkgs.bun ]} \
-      --add-flags "run --prefer-offline --no-install index.ts"
+    runHook preInstall
+
+    mkdir -p $out/bin
+
+    ln -s ${node_modules}/node_modules $out
+    cp -R ./* $out
+
+    # bun is referenced naked in the package.json generated script
+    makeBinaryWrapper ${bun}/bin/bun $out/bin/harmon \
+      --prefix PATH : ${lib.makeBinPath [ bun ]} \
+      --add-flags "run --prefer-offline --no-install --cwd $out ./index.ts"
+
+    runHook postInstall
   '';
 }
--- flake.nix
+++ flake.nix
@@ -2,7 +2,21 @@
   inputs = { utils.url = "github:numtide/flake-utils"; };
   outputs = { self, nixpkgs, utils }:
     utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (self: super: {
+              bun = super.bun.overrideAttrs (final: prev: {
+                src = pkgs.fetchurl {
+                  url =
+                    "https://github.com/oven-sh/bun/releases/download/bun-v${prev.version}/bun-linux-x64-baseline.zip";
+                  hash = "sha256-EKl1neLZfjfSNax2HDFSiO4XGuxMDR6Mom8NnVq7DSY=";
+                };
+              });
+            })
+          ];
+        };
       in {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
